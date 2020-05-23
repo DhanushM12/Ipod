0c1054b752c8a142ab787afe4dd2a9baa834a18b
DhanushM12 <dhanushmukhi@gmail.com>
2020-05-23 22:42:56 +0530

    handle scroll functionality added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Ipod from "./Ipod";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Ipod />
       </div>
     );
--- src/Ipod.js
+++ src/Ipod.js
@@ -1,6 +1,44 @@
 import React from "react";
+import "./index.css";
+import ZingTouch from "zingtouch";
 
 class Ipod extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      display: ["Settings", "Games", "Music", "Cover-Flow"],
+    };
+  }
+  //method for handling the scroll
+  handleScroll = () => {
+    let angle = 0;
+    const myElement = document.getElementById("menu");
+    const region = new ZingTouch.Region(myElement);
+    region.bind(myElement, "rotate", function (e) {
+      angle += e.detail.distanceFromLast;
+
+      if (Math.abs(angle) > 0 && Math.abs(angle) < 15) {
+        let element = document.getElementById("Settings");
+        element.style.background = "blue";
+        element.style.color = "white";
+      }
+      if (Math.abs(angle) > 15 && Math.abs(angle) < 30) {
+        let element = document.getElementById("Games");
+        element.style.background = "blue";
+        element.style.color = "white";
+      }
+      if (Math.abs(angle) > 30 && Math.abs(angle) < 45) {
+        let element = document.getElementById("Music");
+        element.style.background = "blue";
+        element.style.color = "white";
+      }
+      if (Math.abs(angle) > 45 && Math.abs(angle) < 60) {
+        let element = document.getElementById("Cover-Flow");
+        element.style.background = "blue";
+        element.style.color = "white";
+      }
+    });
+  };
   render() {}
 }
 
